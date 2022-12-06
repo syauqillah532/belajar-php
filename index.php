fcde096904fdd704e049631a190f6148c07f5f2d
akhmad syauqillah akmal <akmalasysyauqi@gmail.com>
2022-12-06 09:57:35 +0700

    init project

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+echo "Hello World! <br>" ;
+
+$nama = "Akhmad Syauqillah Akmal" ;
+$umur = "20" ;
+
+echo "Nama saya <strong> $nama </strong>, saya berusia $umur tahun.<br> " ;
+
+$namaAdik = "Adel" ;
+$umurAdik = "19" ;
+
+echo "Nama adik saya <strong> $namaAdik </strong>, adik saya berusia $umurAdik tahun. <br> " ;
+
+$selisihUmur = $umur - $umurAdik ;
+
+echo "Selisih usia saya dan adik saya adalah $selisihUmur tahun. "
+
+?>
